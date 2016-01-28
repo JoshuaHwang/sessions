f12760db119c4c09c65aef64bc57e7664a7cf05e
Joshua Hwang <joshchwang@gmail.com>
2016-01-28 09:43:20 -0800

    edited gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp    = require('gulp');
 var nodemon = require('gulp-nodemon');
 
+var app = 'app/app.js';
+
 gulp.task('default', function() {
   nodemon({
-    script: 'http.js',
+    script: app,
   }).on('start');
 });
