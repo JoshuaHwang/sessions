4317af7dc5c4a84766290985162f40867f614ea6
Joshua Hwang <joshchwang@gmail.com>
2016-02-01 13:26:51 -0800

    (#4) shared JSON data using a factory

--- app/public/js/controllers/sesh.controller.js
+++ app/public/js/controllers/sesh.controller.js
@@ -1,32 +1,8 @@
 
-app.controller('SeshController', function($http) {
-  var vm = this;
+app.controller('SeshController', ['subs', function(subs) {
+  var vm = this;  
 
-  $http.get('./public/submissions.json').success(function(data) {
-    vm.submissions = data;
-  })
-});
-
-/*var theSessions = [
-    {
-      images:      'public/images/vapor-untouchable-2.jpg',
-      name:        'Vapor Untouchable',
-      description: 'Lorem ipsum dore ma proto cato',
-      likes:       540,
-      comments:    39 
-    },
-    {
-      images:      'public/images/air-max-1.jpg',
-      name:        'Air Max 1',
-      description: 'Lorem iasufh dore ma proto cato',
-      likes:       420,
-      comments:    51 
-    },
-    {
-      images:      'public/images/eqt-support.jpg',
-      name:        'EQT Support',
-      description: 'Lorem asdfawfguyewagf dore ma proto cato',
-      likes:       387,
-      comments:    12 
-    }
-  ];*/
+  subs.getSubmissions().then(function(submissions) {
+    vm.submissions = submissions;
+  });
+}]);
