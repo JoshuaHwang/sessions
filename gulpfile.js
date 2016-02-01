29a78c78ec59a6d51198962ababbb0292114897a
Joshua Hwang <joshchwang@gmail.com>
2016-01-31 16:10:56 -0800

    organized angular with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp');
 var jade    = require('gulp-jade');
 var nodemon = require('gulp-nodemon');
+var concat  = require('gulp-concat');
 
 var app = 'app/app.js';
 
@@ -11,14 +12,28 @@ gulp.task('nodemon', function() {
 });
 
 gulp.task('jade', function() {
-  console.log('Running Jade');
+  console.log('Running jade...');
   gulp.src('./template/**/*.jade')
     .pipe(jade())
     .pipe(gulp.dest('./app/views'))
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./template/**/*.jade', ['jade']);
+gulp.task('angular', function() {
+  console.log('Concat angular files..');
+  gulp.src(
+    [
+      './app/public/js/**/module.js',
+      './app/public/js/controllers/**/*.js',
+      './app/public/js/directives/**/*.js',
+      './app/public/js/services/**/*.js'
+    ]
+  ).pipe(concat('all.js'))
+   .pipe(gulp.dest('./app/public'));
 });
 
-gulp.task('default', ['jade', 'watch', 'nodemon']);
+gulp.task('watch', ['jade', 'angular'], function () {
+  gulp.watch('./template/**/*.jade', ['jade']);
+  gulp.watch('./app/public/js/**/*.js', ['angular']);
+});
+
+gulp.task('default', ['jade', 'angular', 'watch', 'nodemon']);
